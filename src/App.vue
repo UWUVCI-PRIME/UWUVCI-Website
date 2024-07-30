f2d603e156c726b6d79aae2b48e4833ae6683a0f
ZestyTS <ZestyTS@gmail.com>
2024-07-30 16:30:26 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Under Construction!" />
+      <HelloWorld msg="Under Construction! or so he says..." />
     </div>
   </header>
 
