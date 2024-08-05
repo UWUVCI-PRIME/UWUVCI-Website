e56f8d20d5bf8393d10068b6c33a40d624d548e1
NicoAICP <24915669+NicoAICP@users.noreply.github.com>
2024-08-06 00:54:55 +0200

    Added Mobile Resizing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,9 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive
+    
+    <v-responsive v-if="isMobile()"
       class="align-centerfill-height mx-auto"
-      max-width="900"
+      max-width="370"
     >
       <v-img
         class="mb-4"
@@ -33,7 +34,53 @@
             </template>
 
             <template #title>
-              <h2 class="text-h5 font-weight-bold" style="text-align:center">The UWUVCI Webpage will soon be available</h2>
+              <h2 class="text-h5 font-weight-bold" style="text-align:center">The UWUVCI Webpage will be <br> available soon</h2>
+            </template>
+
+            <v-overlay
+              opacity=".12"
+              scrim="primary"
+              contained
+              model-value
+              persistent
+            />
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-responsive>
+    <v-responsive v-else
+      class="align-centerfill-height mx-auto"
+      max-width="700"
+    > <v-img
+        class="mb-4"
+        height="150"
+        src="@/assets/UWUVCI_LOGO.png"
+      />
+
+      <div class="text-center">
+        <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+
+        <h1 class="text-h2 font-weight-bold">UWUVCI</h1>
+      </div>
+
+      <div class="py-4" />
+
+      <v-row>
+        <v-col cols="12">
+          <v-card
+            class="py-4"
+            color="surface-variant"
+           
+            
+            rounded="lg"
+            variant="outlined"
+          >
+            <template #image>
+              <v-img position="top right" />
+            </template>
+
+            <template #title>
+              <h2 class="text-h5 font-weight-bold" style="text-align:center">The UWUVCI Webpage will be available soon</h2>
             </template>
 
             <v-overlay
@@ -51,5 +98,13 @@
 </template>
 
 <script setup>
-  //
+
+ function isMobile() {
+   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+     return true
+   } else {
+     return false
+   }
+ }
+
 </script>
