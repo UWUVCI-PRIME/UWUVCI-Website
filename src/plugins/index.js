088138c43b0f669c37e54b51ab1a22ae82db8be2
ZestyTS <ZestyTS@gmail.com>
2024-09-18 14:09:08 -0700

    Added linkify to the registeredPlugins

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,10 +8,12 @@
 import vuetify from './vuetify'
 import pinia from '@/stores'
 import router from '@/router'
+import linkify from './linkify'
 
 export function registerPlugins (app) {
   app
     .use(vuetify)
     .use(router)
     .use(pinia)
+    .use(linkify)
 }
