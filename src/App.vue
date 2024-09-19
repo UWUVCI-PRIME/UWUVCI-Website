0f0040c388e74a9567f824c75142d0e9ee2ca53e
ZestyTS <ZestyTS@gmail.com>
2024-09-18 19:25:45 -0700

    Added footers from different page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <v-app>
     <v-main>
+      <!-- This loads the correct page based on the current route -->
       <router-view />
     </v-main>
 
+    <v-footer height="40" app>
+      <div class="text-caption text-disabled" style="position: absolute; right: 16px;">
+        &copy; {{ new Date().getFullYear() }} <span class="d-sm-inline-block">NicoAICP & ZestyTS</span>
+      </div>
+    </v-footer>
   </v-app>
 </template>
-
-<script setup>
- 
-</script>
 
