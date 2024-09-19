8b19e98eb39a13ee8c48903ee524551e564de74b
ZestyTS <ZestyTS@gmail.com>
2024-09-18 21:04:46 -0700

    Updates

--- assets/main-BpQvj4Ck.js
+++
@@ -1,1 +0,0 @@
-import{c as a,r as p,a as o}from"./VMain-DjgVJina.js";import s from"./index-Cb9V0dcp.js";const r=a(s,o);p(r);r.mount("#app");
--- assets/ndscompat-DPRXDPcB.js
+++
@@ -1,1 +0,0 @@
-import{c as a,r as p,a as o}from"./VMain-DjgVJina.js";import s from"./ndscompat-B2uCf7xb.js";const r=a(s,o);p(r);r.mount("#app");
